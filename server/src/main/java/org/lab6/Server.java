c581b2de80b56ff5679d86210b951a40a98738fb
SkreckOne <gorkov-00-00@mail.ru>
2024-08-27 13:45:20 +0300

    concurent server

--- server/src/main/java/org/lab6/Server.java
+++ server/src/main/java/org/lab6/Server.java
@@ -1,28 +1,93 @@
 package org.lab6;
 
+import common.transfer.Request;
+import common.transfer.Response;
 import common.utils.ArgumentType;
 import common.utils.Command;
 import org.apache.logging.log4j.Logger;
-import common.transfer.Request;
-import common.transfer.Response;
 import org.lab6.commands.Invoker;
 
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.*;
-import java.util.*;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.*;
 
 public class Server {
     private final Selector selector;
     private final DatagramChannel serverSocket;
-    private final Invoker invoker;
     private final Logger logger = ServerMain.logger;
-    private final ByteBuffer buffer;
-    private boolean running = true;
+    private final ByteBuffer receiveBuffer;
+    private final ByteBuffer sendBuffer;
+    private final Object receiveBufferLock = new Object();
+    private final Object sendBufferLock = new Object();
+    private volatile boolean running = true;
+
+    private final ExecutorService acceptExecutor = new ForkJoinPool();
+    private final ExecutorService processExecutor = Executors.newCachedThreadPool();
+    private final ExecutorService sendExecutor = new ForkJoinPool();
+    private final Invoker invoker;
+
+    private final BlockingQueue<RequestContext> requestQueue = new LinkedBlockingQueue<>();
+    private final BlockingQueue<ResponseContext> responseQueue = new LinkedBlockingQueue<>();
+
+    private static class ResponseContext {
+        private final Response response;
+        private final DatagramChannel channel;
+        private final InetSocketAddress clientAddress;
+
+        public ResponseContext(Response response, DatagramChannel channel, InetSocketAddress clientAddress) {
+            this.response = response;
+            this.channel = channel;
+            this.clientAddress = clientAddress;
+        }
+
+        public Response getResponse() {
+            return response;
+        }
+
+        public DatagramChannel getChannel() {
+            return channel;
+        }
+
+        public InetSocketAddress getClientAddress() {
+            return clientAddress;
+        }
+    }
+
+    private static class RequestContext {
+        private final Request request;
+        private final DatagramChannel channel;
+        private final InetSocketAddress clientAddress;
+
+        public RequestContext(Request request, DatagramChannel channel, InetSocketAddress clientAddress) {
+            this.request = request;
+            this.channel = channel;
+            this.clientAddress = clientAddress;
+        }
+
+        public Request getRequest() {
+            return request;
+        }
+
+        public DatagramChannel getChannel() {
+            return channel;
+        }
+
+        public InetSocketAddress getClientAddress() {
+            return clientAddress;
+        }
+    }
 
     public Server(int port, Invoker invoker) throws IOException {
-        this.buffer = ByteBuffer.allocate(4096);
+        this.receiveBuffer = ByteBuffer.allocate(4096);
+        this.sendBuffer = ByteBuffer.allocate(4096);
         this.selector = Selector.open();
         this.serverSocket = DatagramChannel.open();
         this.serverSocket.bind(new InetSocketAddress(port));
@@ -34,55 +99,15 @@ public class Server {
     public void run() throws IOException {
         logger.info("UDP Server started on port " + serverSocket.socket().getLocalPort());
 
+        acceptExecutor.submit(this::acceptRequests);
+        processExecutor.submit(this::processRequests);
+        sendExecutor.submit(this::sendResponses);
+
         while (running) {
-            selector.select();
-            Set<SelectionKey> selectedKeys = selector.selectedKeys();
-            Iterator<SelectionKey> iter = selectedKeys.iterator();
-
-            while (iter.hasNext()) {
-                SelectionKey key = iter.next();
-
-                if (key.isReadable()) {
-                    handleRequest(key);
-                }
-
-                iter.remove();
-            }
             executeServerCommand();
         }
+
         stop();
-    }
-
-    private void handleRequest(SelectionKey key) {
-        try {
-            DatagramChannel channel = (DatagramChannel) key.channel();
-
-            buffer.clear();
-            InetSocketAddress clientAddress = (InetSocketAddress) channel.receive(buffer);
-            if (clientAddress == null) {
-                return;
-            }
-
-            buffer.flip();
-            Request request = deserializeRequest();
-            if (request == null) {
-                return;
-            }
-            logger.info("Received request from client: " + request);
-
-            sendResponse(channel, clientAddress, new Response(Response.ResponseType.ACK, true, "ACK"));
-
-            Response response;
-            if (request.getRequestType() != Request.RequestType.LOCAL) {
-                response = invoker.handle(request);
-            } else {
-                response = new Response(Response.ResponseType.DEFAULT, false, "Invalid request type");
-            }
-
-            sendResponse(channel, clientAddress, response);
-        } catch (IOException e) {
-            logger.error("Failed to handle request", e);
-        }
     }
 
     private void executeServerCommand() {
@@ -108,9 +133,89 @@ public class Server {
         }
     }
 
-    private Request deserializeRequest() throws IOException {
+    private void acceptRequests() {
+        while (running) {
+            try {
+                selector.select();
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iter = selectedKeys.iterator();
+
+                while (iter.hasNext()) {
+                    SelectionKey key = iter.next();
+
+                    if (key.isReadable()) {
+                        DatagramChannel channel = (DatagramChannel) key.channel();
+                        InetSocketAddress clientAddress;
+
+                        synchronized (receiveBufferLock) {
+                            receiveBuffer.clear();
+                            clientAddress = (InetSocketAddress) channel.receive(receiveBuffer);
+                            if (clientAddress == null) {
+                                continue;
+                            }
+
+                            receiveBuffer.flip();
+                        }
+                        Request request = deserializeRequest(receiveBuffer);
+                        if (request == null) {
+                            continue;
+                        }
+                        logger.info("Received request from client: " + request);
+
+                        requestQueue.put(new RequestContext(request, channel, clientAddress));
+                    }
+
+                    iter.remove();
+                }
+            } catch (IOException | InterruptedException e) {
+                logger.error("Failed to accept request", e);
+            }
+        }
+    }
+
+    private void processRequests() {
+        while (running) {
+            try {
+                RequestContext requestContext = requestQueue.take();
+                Request request = requestContext.getRequest();
+
+
+                Response response = processRequest(request);
+
+                responseQueue.put(new ResponseContext(response, requestContext.getChannel(), requestContext.getClientAddress()));
+            } catch (InterruptedException e) {
+                logger.error("Processing interrupted", e);
+            }
+        }
+    }
+
+    private void sendResponses() {
+        while (running) {
+            try {
+                ResponseContext responseContext = responseQueue.take();
+                Response response = responseContext.getResponse();
+                DatagramChannel channel = responseContext.getChannel();
+                InetSocketAddress clientAddress = responseContext.getClientAddress();
+
+
+                Response ackResponse = new Response(Response.ResponseType.ACK, true, "ACK");
+                sendResponse(channel, clientAddress, ackResponse);
+
+                sendResponse(channel, clientAddress, response);
+            } catch (InterruptedException | IOException e) {
+                logger.error("Failed to send response", e);
+            }
+        }
+    }
+
+    private Response processRequest(Request request) {
+        return invoker.handle(request);
+    }
+
+    private Request deserializeRequest(ByteBuffer buffer) throws IOException {
         byte[] bytes = new byte[buffer.remaining()];
         buffer.get(bytes);
+
 
         ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
         try (ObjectInputStream ois = new ObjectInputStream(bis)) {
@@ -127,17 +232,20 @@ public class Server {
             oos.writeObject(response);
         }
 
-        buffer.clear();
-        buffer.put(bos.toByteArray());
-        buffer.flip();
 
-        channel.send(buffer, address);
+        synchronized (sendBufferLock) {
+            sendBuffer.clear();
+            sendBuffer.put(bos.toByteArray());
+            sendBuffer.flip();
+        }
+
+        channel.send(sendBuffer, address);
         logger.info("Sent response to client: " + response);
     }
 
     public void stop() {
         running = false;
-        try {
+        acceptExecutor.shutdown(); try {
             serverSocket.close();
             selector.close();
         } catch (IOException e) {
